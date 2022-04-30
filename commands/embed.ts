f925046038a600ac8a2bc9146dcd3f836bce95b8
Brycked <brycked@outlook.com>
2022-04-30 18:43:51 +0200

    Make embed customizable

--- commands/embed.ts
+++ commands/embed.ts
@@ -7,35 +7,7 @@ export default {
     permissions: ['ADMINISTRATOR'],
     testOnly: true,
 
-    callback: async ({ message }) => {
-        const embed =  new MessageEmbed({
-            title: 'Title',
-            description: 'Description',
-            color: 'BLURPLE',
-            footer: {text: 'Footer'},
-            fields: [
-                {
-                    name: 'Field 1',
-                    value: 'Description 1',
-                    inline: true
-                },
-                
-                {
-                    name: 'Field 2',
-                    value: 'Description 2',
-                    inline: true
-                }
-            ]
-        })
-
-        const sent = await (message).reply({
-            embeds: [embed]
-        })
-
-        await new Promise(resolve => setTimeout(resolve, 5000))
-
-        sent.edit({
-            embeds: [embed.setTitle('New Title')]
-        })
+    callback: async ({ text }) => {
+        return new MessageEmbed(JSON.parse(text))
     }
 } as ICommand

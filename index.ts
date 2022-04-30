a7db8f4bad30de0e7c379a3569ee06cad3e208b5
Brycked <brycked@outlook.com>
2022-04-30 13:27:29 +0200

    Add basic slash commands

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import Discord, { Intents } from 'discord.js'
+import Discord, { Client, Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
+
 
 const client = new Discord.Client({
     intents: [
@@ -11,11 +12,54 @@ const client = new Discord.Client({
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user?.tag}!`)
+    const guildId = '968171159776559174'
+    const guild = client.guilds.cache.get(guildId)
+    let commands = guild ? guild.commands : client.application?.commands!
+
+    commands.create({
+        name: 'ping',
+        description: 'Replies with 🏓 *Pong!*'
+    })
+
+    commands.create({
+        name: 'say',
+        description: 'Let the bot say something',
+        options: [
+            {
+                name: 'text',
+                description: 'The text to say',
+                required: true,
+                type: Discord.Constants.ApplicationCommandOptionTypes.STRING
+            }
+        ]
+    })
 })
 
 client.on('messageCreate', (message) => {
     if (message.content.startsWith('ping'))
         message.reply('🏓 *Pong!*')
+})
+
+client.on('interactionCreate', (inter) => {
+    if (!inter.isCommand()) return
+
+    const {commandName, options} = inter
+
+    switch (commandName) {
+        case 'ping':
+            inter.reply({
+                content: '🏓 *Pong!*',
+                ephemeral: true
+            })
+            break
+        case 'say':
+            const text = options.getString('text')!
+            inter.reply({
+                content: text
+            })
+            break   
+    }
+
 })
 
 client.login(process.env.TOKEN)

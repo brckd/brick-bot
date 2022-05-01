d0437c597f10e52b6f887c19e56f7ba8ef4cacc3
Brycked <brycked@outlook.com>
2022-05-01 09:25:28 +0200

    removed unnecessary testOnly param

--- commands/embed.ts
+++ commands/embed.ts
@@ -5,7 +5,6 @@ export default {
     category: 'testing',
     description: 'Sends an embed',
     permissions: ['ADMINISTRATOR'],
-    testOnly: true,
 
     callback: async ({ text }) => {
         return new MessageEmbed(JSON.parse(text))

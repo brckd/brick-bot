ede86a07af670b52dc0b4d2b7f365c833f109932
Brycked <brycked@outlook.com>
2022-05-01 10:48:52 +0200

    Replace constants with strings

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import Discord, { Client, Constants, Intents } from 'discord.js'
+import Discord from 'discord.js'
 import WOKCommands from 'wokcommands'
 import path from 'path'
 import 'dotenv/config'
@@ -6,12 +6,12 @@ import 'dotenv/config'
 
 const client = new Discord.Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        'GUILDS',
+        'GUILD_MESSAGES',
+        'GUILD_MESSAGE_REACTIONS'
     ],
     partials: [
-        Constants.PartialTypes.REACTION
+        'REACTION'
     ]
 })
 

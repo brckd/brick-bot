4e4a3299ea15c26be368f2775468daadd913a270
Brycked <brycked@outlook.com>
2022-05-02 10:56:18 +0200

    Move to Sapphire framework

--- index.ts
+++ index.ts
@@ -1,32 +1,13 @@
-import Discord from 'discord.js'
-import WOKCommands from 'wokcommands'
-import path from 'path'
+import { SapphireClient } from '@sapphire/framework'
 import 'dotenv/config'
 
 
-const client = new Discord.Client({
+const client = new SapphireClient({
     intents: [
         'GUILDS',
-        'GUILD_MESSAGES',
-        'GUILD_MEMBERS',
-        'GUILD_MESSAGE_REACTIONS'
+        'GUILD_MESSAGES'
     ],
-    partials: [
-        'REACTION'
-    ]
-})
-
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user?.tag}!`)
-
-    new WOKCommands(client, {
-        commandDir: path.join(__dirname, 'commands'),
-        featureDir: path.join(__dirname, 'features'),
-        typeScript: true,
-        testServers: ['968171159776559174'],
-        botOwners: ['691572882148425809'],
-        mongoUri: process.env.MONGO_URI
-    })
+    defaultPrefix: '!'
 })
 
 client.login(process.env.TOKEN)

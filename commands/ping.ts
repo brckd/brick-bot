f7f6ec7ce232b4fb75f909589cadd3e8406b1c75
Brycked <brycked@outlook.com>
2022-05-02 11:07:36 +0200

    Add ping command

---
+++ commands/ping.ts
@@ -0,0 +1,17 @@
+import { Command } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class Ping extends Command {
+    public constructor(context: Command.Context, options: Command.Options) {
+        super(context, {
+            ...options,
+            name:'ping',
+            aliases: ['pong'],
+            description: ''
+        })
+    }
+
+    public async messageRun(message: Message) {
+        await message.channel.send(`🏓 *Pong!*\nBot Latency: **${Math.round(this.container.client.ws.ping)}**ms`)
+      }
+}

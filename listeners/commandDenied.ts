767e8bcf414e90cf29694b8b608791f8d9e9cf96
Brycked <brycked@outlook.com>
2022-05-02 13:16:32 +0200

    Add commandDenied listener

---
+++ listeners/commandDenied.ts
@@ -0,0 +1,9 @@
+import type { UserError, CommandDeniedPayload } from '@sapphire/framework';
+import { Listener } from '@sapphire/framework';
+
+export class CommandDeniedListener extends Listener {
+  public run(error: UserError, { message }: CommandDeniedPayload) {
+    if (Reflect.get(Object(error.context), 'silent')) return
+    return message.channel.send(error.message);
+  }
+}

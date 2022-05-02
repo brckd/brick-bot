9faa7e9c4c01de9c2d78047ad47bd28e0423d90c
Brycked <brycked@outlook.com>
2022-05-02 13:22:53 +0200

    Better class name

--- listeners/commandDenied.ts
+++ listeners/commandDenied.ts
@@ -1,7 +1,7 @@
 import type { UserError, CommandDeniedPayload } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
 
-export class CommandDeniedListener extends Listener {
+export class CommandDenied extends Listener {
   public run(error: UserError, { message }: CommandDeniedPayload) {
     if (Reflect.get(Object(error.context), 'silent')) return
     return message.channel.send(error.message);

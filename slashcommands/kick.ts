c2c759698e5ab6a647e36284d58b5af3c289261b
Brycked <brycked@outlook.com>
2022-05-04 00:39:24 +0200

    Add kick slashcommand

---
+++ slashcommands/kick.ts
@@ -0,0 +1,36 @@
+import { GuildMember } from "discord.js";
+import { SlashCommandTemplate } from "../handlers";
+
+const durations = [
+    {name: '60 seconds', value: 1000 * 60},
+    {name: '5 Minutes', value: 1000 * 60 * 5},
+    {name: '10 Minutes', value: 1000 * 60 * 10},
+    {name: '30 Minutes', value: 1000 * 60 * 30},
+    {name: '1 Hour', value: 1000 * 60 * 60},
+    {name: '1 Week', value: 1000 * 60 * 60 * 24 * 7},
+]
+
+export default {
+    description: 'Kick user',
+    permissions: ['KICK_MEMBERS'],
+
+    options: [
+        {
+            name: 'user',
+            description: 'The user to kick',
+            type: 'USER',
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'The reason for kicking, if any',
+            type: 'STRING',
+            required: false
+        }
+    ],
+
+    run: ({ interaction }, member: GuildMember, reason?: string) => {
+        member.kick(reason)
+        interaction.reply(`${member.user.tag} has been kicked ${reason?'for reason:\n> '+reason:''}`)
+    }
+} as SlashCommandTemplate

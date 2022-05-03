81a90909b40482d3d0f52263e6331dc1de22ce3d
Brycked <brycked@outlook.com>
2022-05-04 00:39:33 +0200

    Add ban slashcommand

---
+++ slashcommands/ban.ts
@@ -0,0 +1,51 @@
+import { GuildMember } from "discord.js";
+import { SlashCommandTemplate } from "../handlers";
+
+const durations = [
+    {name: 'Don\' Delete Any', value: 0},
+    {name: 'Previous 24 Hours', value: 1000 * 60 * 60 * 24},
+    {name: 'Previous 7 Days', value: 1000 * 60 * 60 * 24 * 7}
+]
+
+export default {
+    description: 'Ban user',
+    permissions: ['BAN_MEMBERS'],
+    guildOnly: true,
+
+    options: [
+        {
+            name: 'user',
+            description: 'The user to ban',
+            type: 'USER',
+            required: true
+        },
+        {
+            name: 'delete_messages',
+            description: 'How much of their recent message history to delete',
+            type: 'NUMBER',
+            choices: durations,
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'The reason for banning, if any',
+            type: 'STRING',
+            required: false
+        }
+    ],
+
+    run: ({ interaction }, member: GuildMember, delete_messages: Number, reason?: string) => {
+        if (!member) 
+            return interaction.reply('Invalid user')
+
+        const user = member.user
+
+        interaction.guild?.bans.create(
+            member,
+            {days: 7, reason: reason}
+        )
+
+        interaction.reply(`${user.tag} has been banned ${reason?'for reason:\n> '+reason:''}
+        ${delete_messages>0 ? durations.find(d=>delete_messages===d.value)?.name + ' of message history have been deleted' : ''}`)
+    }
+} as SlashCommandTemplate

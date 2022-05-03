76869f060be3fb458bdef29b6e0792e7627ca3d7
Brycked <brycked@outlook.com>
2022-05-03 12:55:01 +0200

    Make ping quoted

--- commands/info/ping.ts
+++ commands/info/ping.ts
@@ -2,6 +2,6 @@ import { ICommand } from "../../handlers"
 
 export default {
     run: async ({ client, message }) => {
-        message.reply(`🏓 *Pong!*\nBot Latency: **${Math.round(client.ws.ping)}**ms`)
+        message.reply(`> 🏓 *Pong!*\nBot Latency: **${Math.round(client.ws.ping)}**ms`)
     }
 } as ICommand

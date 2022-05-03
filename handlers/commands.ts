e284794b7bc179f0308d80cc4e954bafdba07e63
Brycked <brycked@outlook.com>
2022-05-03 13:16:42 +0200

    Prettify

--- handlers/commands.ts
+++ handlers/commands.ts
@@ -4,18 +4,21 @@ import { Client } from '.'
 
 export default (client: Client, reload: boolean) => {
     const ending = '.ts'
+
     fs.readdirSync('commands/').forEach((category) => {
         const files = getFiles(`commands/${category}`, ending)
 
         files.forEach((f) => {
-            if (reload) {
+            if (reload)
                 delete require.cache[require.resolve(`commands/${category}/${f}`)]
-            }
+
             const command = require(`../commands/${category}/${f}`).default
             command.name = command.name || f.slice(0, f.length-ending.length)
             command.category = command.category || category
+
             client.commands.set(command.name, command)
         })
     })
+
     console.log(`Loaded ${client.commands.size} command${client.commands.size===1?'':'s'}`)
 }

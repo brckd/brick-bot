73f416c1e0cb88ca7c70d44a1c978f28b04691ff
Brycked <brycked@outlook.com>
2022-05-03 14:56:35 +0200

    Add ping slashcommand

---
+++ slashcommands/ping.ts
@@ -0,0 +1,12 @@
+import { ISlashCommand } from "../handlers";
+
+export default {
+    name: 'ping',
+    description: 'Replies with 🏓 *Ping!* and the bot latency',
+    run: ({ client, interaction }) => {
+        interaction.reply({
+            content: `> 🏓 *Pong!*\nBot Latency: **${Math.round(client.ws.ping)}**ms`,
+            ephemeral: true
+        })
+    }
+} as ISlashCommand

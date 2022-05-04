aec582730acc85d72c416cc38f372e07200ebd15
Brycked <brycked@outlook.com>
2022-05-04 11:47:29 +0200

    Remove name

--- slashcommands/ping.ts
+++ slashcommands/ping.ts
@@ -1,8 +1,8 @@
 import { SlashCommandTemplate } from "../handlers";
 
 export default {
-    name: 'ping',
     description: 'Replies with 🏓 *Ping!* and the bot latency',
+
     run: ({ client, interaction }) => {
         interaction.reply({
             content: `> 🏓 *Pong!*\nBot Latency: **${Math.round(client.ws.ping)}**ms`,

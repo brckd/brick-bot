f5b60a033e75e3dd10ee1f485cc2c9828119babe
Brycked <brycked@outlook.com>
2022-05-05 15:52:26 +0200

    Add mention prefix

--- events/ready.ts
+++ events/ready.ts
@@ -3,5 +3,6 @@ import { EventTemplate } from "../handlers";
 export default {
     run: (client) => {
         console.log(`Logged in as ${client.user?.tag}`)
+        client.prefix.push(client.user!.toString())
     }
 } as EventTemplate

e744f21849ac3c8a6c44ab157c51571d57822c0d
Brycked <brycked@outlook.com>
2022-05-08 10:42:15 +0200

    Add uptime command

---
+++ commands/info/uptime.ts
@@ -0,0 +1,10 @@
+import { CommandTemplate } from "../../handlers"
+
+export default {
+    types: ['LEGACY', 'SLASH'],
+    description: 'Replies with the bot\'s uptime',
+    
+    run: ({ client, reply }) => {
+        reply(`Online <t:${Math.round(client.readyTimestamp!/1000)}:R>`)
+    }
+} as CommandTemplate

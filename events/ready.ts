24e0af6639dead0b3c50f6267417b8993a55cf85
Brycked <brycked@outlook.com>
2022-05-08 10:59:02 +0200

    Add presence status

--- events/ready.ts
+++ events/ready.ts
@@ -4,5 +4,16 @@ export default {
     run: (client) => {
         console.log(`Logged in as ${client.user?.tag}`)
         client.prefix.push(client.user!.toString())
+
+        client.user?.setPresence({activities: [{
+            type: 'PLAYING',
+            name: 'Connecting'
+        }]})
+        setTimeout(() => {
+            client.user?.setPresence({activities: [{
+                type: 'LISTENING',
+                name: '!help'
+            }]})
+        }, 1000 * 5)
     }
 } as EventTemplate
